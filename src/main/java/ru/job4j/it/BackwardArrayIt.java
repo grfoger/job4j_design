6cf7f4ecae8932de904541e2acf964c230b85c1f
Arhip <grfoger@gmail.com>
2022-01-12 18:19:03 +0300

    corrected mistake

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -9,14 +9,13 @@ public class BackwardArrayIt implements Iterator<Integer> {
 
     public BackwardArrayIt(int[] data) {
         this.data = data;
-        if (data.length != 0) {
-            this.point = data.length - 1;
-        }
+        this.point = data.length - 1;
+
     }
 
     @Override
     public boolean hasNext() {
-        return point < data.length;
+        return point >= 0;
     }
 
     @Override
