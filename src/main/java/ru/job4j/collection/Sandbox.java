c5ea2a952a94765eb02e740afe4b08ed1417dc8e
grfoger <grfoger@gmail.com>
2022-01-23 20:29:37 +0300

    SimpleQueue init [#160]

---
+++ src/main/java/ru/job4j/collection/Sandbox.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import ru.job4j.collection.list.SimpleLinkedList;
+import ru.job4j.it.MatrixIt;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        SimpleStack<Integer> stack = new SimpleStack<>();
+
+        System.out.println(stack.pop());
+
+
+
+    }
+}
