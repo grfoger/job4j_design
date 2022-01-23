47ed5dfc92f4c92acbd8e666c605abd577c1889e
grfoger <grfoger@gmail.com>
2022-01-23 15:56:43 +0300

    addFirst method corrected #2 [#71474]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -31,7 +31,7 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public void addFirst(T value) {
-        head = new Node<>(value, head);;
+        head = new Node<>(value, head);
     }
 
     @Override
