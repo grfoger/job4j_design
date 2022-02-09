703b5a0db127190da83b9487453592b74220fcd2
Arhip <grfoger@gmail.com>
2022-02-09 11:12:50 +0300

    Question corrected [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -7,19 +7,11 @@ import java.util.stream.Collectors;
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        int added = 0;
+
         int changed = 0;
         int deleted = 0;
 
-        Map<Integer, String> mapPrevious = previous.stream().collect(Collectors.toMap(User::getId, User::getName));
         Map<Integer, String> mapCurrent = current.stream().collect(Collectors.toMap(User::getId, User::getName));
-
-
-        for (User user : current) {
-            if (!previous.contains(user)) {
-                added++;
-            }
-        }
 
         for (User user : previous) {
             if (mapCurrent.get(user.getId()) != null && !user.getName().equals(mapCurrent.get(user.getId()))) {
@@ -29,6 +21,7 @@ public class Analize {
                 deleted++;
             }
         }
+        int added = current.size() - previous.size() + deleted;
 
         return new Info(added, changed, deleted);
     }
