e30f9b4ca5799e2a3bb6e69be2bd8646044a6b2b
Arhip <grfoger@gmail.com>
2022-02-10 10:11:11 +0300

    3.0. Тестирование IO [#173905] - init

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -16,23 +16,22 @@ public class AnalizyTest {
 
     @Test
     public void analyze() throws IOException {
-
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(result::append);
+        }
+        assertThat(result.toString(), is("10:57:01;10:59:01;11:01:02;11:02:02;"));
     }
-
-//    @Test
-//    public void drop() throws IOException {
-//        File source = folder.newFile("source.txt");
-//        File target = folder.newFile("target.txt");
-//        try (PrintWriter out = new PrintWriter(source)) {
-//            out.println("hello foolish dude");
-//            out.println("java job4j php");
-//        }
-//        Abuse.drop(source.getAbsolutePath(), target.getAbsolutePath(), List.of("foolish", "php"));
-//        StringBuilder rsl = new StringBuilder();
-//        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-//            in.lines().forEach(rsl::append);
-//        }
-//        assertThat(rsl.toString(), is("hello dude java job4j "));
-//    }
-
 }
