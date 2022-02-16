9967359bb3305cee0ccfab6a416e3d5ec77ebae1
Arhip <grfoger@gmail.com>
2022-02-16 09:15:52 +0300

    3. Бот [#7921] - init

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,12 +13,15 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("/?msg=Bye")) {
+                    String str = in.readLine();
+                        if (str.contains("/?msg=Exit")) {
                             server.close();
+                        } else if (str.contains("/?msg=Hello")) {
+                            System.out.println("Hello world");
+                        } else {
+                            System.out.println("What?");
                         }
-                        System.out.println(str);
-                    }
+
                     out.flush();
                 }
             }
