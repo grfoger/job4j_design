ac89b3f3beacc5472d7ea4d68db6f533340b79bf
grfoger <grfoger@gmail.com>
2022-02-22 22:35:12 +0300

    1. Схема прав пользователи и роли. [#1093] - init

---
+++ sql_scripts/insert.sql
@@ -0,0 +1,29 @@
+insert into users(name) values ('Holmes');
+insert into users(name) values ('Watson');
+
+insert into role(name) values ('detective');
+insert into role(name) values ('doctor');
+
+insert into rules(name) values ('investigation');
+insert into rules(name) values ('heal');
+insert into rules(name) values ('chemical');
+
+insert into role_rules(id_role, id_rules) values (1, 1);
+insert into role_rules(id_role, id_rules) values (1, 3);
+insert into role_rules(id_role, id_rules) values (2, 2);
+insert into role_rules(id_role, id_rules) values (2, 3);
+
+insert into item(name, id_users, id_category, id_state) values ('catch dog', 1, 2, 3);
+insert into item(name, id_users, id_category, id_state) values ('heal sir Henry', 2, 1, 2);
+
+insert into comments(name, id_item) values ('use oatmeal porridge', 2);
+
+insert into attachs(name, id_item) values ('evidence', 1);
+
+insert into state(name) values ('waitnig');
+insert into state(name) values ('in progress');
+insert into state(name) values ('complete');
+
+insert into category(name) values ('not urgent');
+insert into category(name) values ('urgently');
+
