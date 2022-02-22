177074ea6e65c4bd0bb011a65805021bf2ba4142
Arhip <grfoger@gmail.com>
2022-02-22 16:02:24 +0300

    1. Схема прав пользователи и роли. [#1093] - base

---
+++ sql_scripts/create.sql
@@ -0,0 +1,51 @@
+create table users(
+     id serial primary key,
+     name varchar(255),
+     id_role int references role(id)
+ );
+
+create table role(
+     id serial primary key,
+     name varchar(255)
+ );
+
+create table role_rules(
+     id serial primary key,
+     id_role int references role(id),
+     id_rules int references rules(id)
+ );
+
+create table rules(
+     id serial primary key,
+     name varchar(255)
+ );
+
+create table item(
+     id serial primary key,
+     name varchar(255),
+     id_users int references users(id),
+     id_category int references category(id),
+     id_state int references state(id)
+ );
+
+create table comments(
+     id serial primary key,
+     name varchar(255),
+     id_item int references item(id)
+ );
+
+create table attachs(
+     id serial primary key,
+     name varchar(255),
+     id_item int references item(id)
+ );
+
+create table state(
+     id serial primary key,
+     name varchar(255)
+ );
+
+create table category(
+     id serial primary key,
+     name varchar(255)
+ );
