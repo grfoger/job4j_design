c261aab531b86804ff0687fc8ba67ff8fbcc431d
Arhip <grfoger@gmail.com>
2022-03-11 15:22:14 +0300

    RefDemo - base

--- src/main/java/ru/job4j/io/finder/Sandbox.java
+++ src/main/java/ru/job4j/io/finder/Sandbox.java
@@ -4,14 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Sandbox {
 
     public static void main(String[] args) {
-        String one = "h*sbhfd?";
-        one = one.replace("*", "111");
-        one = one.replace("?", "222");
-        System.out.println(one);
+        Map<String, String> map = new HashMap<>();
+        System.out.println(map.get(""));
     }
 }
 
